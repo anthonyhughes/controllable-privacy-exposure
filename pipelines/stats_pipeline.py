710c77520526da80449425437144536fa950695c
Anthony Hughes <ajhughes3@sheffield.ac.uk>
2024-09-10 16:34:11 +0100

    Added a pipeline for generating stats

--- pipelines/stats_pipeline.py
+++ pipelines/stats_pipeline.py
@@ -1,59 +1,48 @@
+# code reused from https://github.com/casszhao/PruneHall
+
 import json
-import os
+import nltk
 from nltk import word_tokenize
 import numpy as np
-
-from constants import SUMMARY_TYPES
+from constants import SUMMARY_TYPES, EXAMPLES_ROOT
 
 data_stats = {}
 
-if __name__ == '__main__':
 
-    for dataset in SUMMARY_TYPES[2:3]:
+def run(task):
 
-        # go down to the individual prompt results
-        fname = os.path.join(
-            'data',
-            dataset + '.json'
+    fname = f"{EXAMPLES_ROOT}legal_court/tldrlegal_v1.json"
+
+    with open(fname, "r", encoding="utf8") as f:
+        data = json.load(f)
+
+    word_lengths = np.zeros(len(data))
+    reference_lengths = np.zeros(len(data))
+
+    for i, (key, value) in enumerate(data.items()):
+
+        reference = (
+            value.get("reference_summary", None)
+            or value.get("claim", None)
+            or value.get("target", None)
         )
 
-        COUNTER = 0
+        word_lengths[i] = len(word_tokenize(value["original_text"]))
+        if reference is not None:
+            reference_lengths[i] = len(word_tokenize(reference))
 
-        with open(fname, 'r', encoding='utf8') as f:
-            data = json.load(f)
+    data_stats[task] = {
+        "dataset_size": len(data),
+        "source_mean": round(word_lengths.mean(), 1),
+        "source_max": round(word_lengths.max(), 1),
+        "reference_mean": round(reference_lengths.mean(), 1),
+        "reference_max": round(reference_lengths.max(), 1),
+    }
 
-        word_lengths = np.zeros(len(data))
-        reference_lengths = np.zeros(len(data))
-
-        for i, (key, value) in enumerate(data.items()):
-
-            if isinstance(value['document'], str) is False:
-                COUNTER += 1
-                continue
-
-            if len(value['document'].split()) < 5:
-                COUNTER += 1
-                continue
-
-            reference = value.get("reference_summary", None) or value.get("claim", None) or value.get("target", None)
-
-            word_lengths[i] = len(word_tokenize(value['document']))
-            if reference is not None:
-                reference_lengths[i] = len(word_tokenize(reference))
+    with open("data/stats.json", "w", encoding="utf8") as f:
+        json.dump(data_stats, f, indent=4, default=str)
 
 
-        data_stats[dataset] = {
-            'dataset_size': len(data),
-            'source_mean': round(word_lengths.mean(), 1),
-            'source_max': round(word_lengths.max(), 1),
-            "reference_mean": round(reference_lengths.mean(), 1),
-            'reference_max': round(reference_lengths.max(), 1),
-        }
-
-    with open('data/stats.json', 'w', encoding='utf8') as f:
-        json.dump(
-            data_stats,
-            f,
-            indent=4,
-            default=str
-        )
+if __name__ == "__main__":
+    nltk.download("punkt")
+    run("legal_court")
