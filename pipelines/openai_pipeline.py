de3bb8a3bd359baadc57557c20949ebd2d1edd1a
Anthony Hughes <ajhughes3@sheffield.ac.uk>
2024-09-11 16:19:48 +0100

    Removed result

--- pipelines/openai_pipeline.py
+++ pipelines/openai_pipeline.py
@@ -132,5 +132,3 @@ if __name__ == "__main__":
             hadm_ids=target_admission_ids,
             tasks=[task],
         )
-
-{'role': 'user', 'content': '\n                ### Instruction:\n                \n        Summarise the document.\n  ...                ### Response:\n            '}
