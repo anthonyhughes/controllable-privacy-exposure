283a78307bb8dc61bdbaf8f98afa4f04421f0301
anthonyhughes <anthony.hughes@datalanguage.com>
2024-09-15 07:28:56 +0100

    Updates to the eval pipeline

--- pipelines/eval_pipeline.py
+++ pipelines/eval_pipeline.py
@@ -3,12 +3,10 @@ from constants import (
     SUMMARY_TYPES,
 )
 from evaluate import load
-
-from utils.pii_eval import run_privacy_eval
 from timeit import default_timer as timer
 import argparse
 
-from utils.utility_utils import run_utility_eval, print_results_to_latex
+from utils.utility_utils import run_utility_eval
 
 bertscore = load("bertscore")
 rouge_eval = load("rouge")
@@ -20,26 +18,31 @@ if __name__ == "__main__":
     parser.add_argument(
         "-m", "--model", help="Choose a model for inference", default=EVAL_MODELS
     )
+    all_types = SUMMARY_TYPES + ["all"]
     parser.add_argument(
         "-t",
         "--tasks",
         help="Choose a model for inference",
         default=SUMMARY_TYPES,
-        choices=SUMMARY_TYPES,
+        choices=all_types,
     )
+
     args = parser.parse_args()
 
     models = args.model
 
     if args.tasks:
         print(f"Target model is {args.tasks}")
-        tasks = [args.tasks]
+        if args.tasks == "all":
+            tasks = SUMMARY_TYPES
+        else:
+            tasks = [args.tasks]
 
     start = timer()
     for target_model in [models]:
         print(f"Running evaluation pipeline for model: {target_model}")
         run_utility_eval(target_model=target_model, tasks=tasks)
-        run_privacy_eval(target_model=target_model, tasks=tasks)
+        # run_privacy_eval(target_model=target_model, tasks=tasks)
         end_m = timer() - start
         print(f"Model time to complete in secs: {end_m}")
     end = timer() - start
